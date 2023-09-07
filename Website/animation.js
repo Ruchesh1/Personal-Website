28758b5e88a5055b0fbe8e87c93ee2ae33f3096c
Ruchesh Kapadia <rucheshkapadia@gmail.com>
2023-09-06 20:55:53 -0400

    Add files via upload
    updated to bootstrap 5

---
+++ Website/animation.js
@@ -0,0 +1,29 @@
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+// ..
+const $backTop = $(".back-to-top");
+const isHidden = "is-hidden";
+
+AOS.init({
+    offset: 200,
+    delay: 50,
+    once: true
+});
+
+/*$('a[data-toggle="pill"]').on("shown.bs.tab", e => {
+  AOS.refresh();
+});*/
+
+$(window).on("scroll", function () {
+    const $this = $(this);
+    if ($this.scrollTop() + $this.height() == $(document).height()) {
+        $backTop.removeClass(isHidden);
+    } else {
+        $backTop.addClass(isHidden);
+    }
+});
+
+$backTop.on("click", () => {
+    $("html, body").animate({ scrollTop: 0 }, "slow");
+    return false;
+});
